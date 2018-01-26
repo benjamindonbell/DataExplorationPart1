8f2fd398cb5ca4d0e39bde36fa5d290bb98d9d83
ben bell <benjamindonbell@yahoo.com>
2018-01-25 18:23:20 -0800

    This Branch doesn't work

--- MVC-DataExploration/Controllers/ProductController.cs
+++ MVC-DataExploration/Controllers/ProductController.cs
@@ -3,15 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using MVC_DataExploration.Models;
 namespace MVC_DataExploration.Controllers
 {
     public class ProductController : Controller
     {
+        MyDBContext DB = new MyDBContext();
         // GET: Product
         public ActionResult Index()
         {
-            return View();
+            return View(DB.Products);
         }
     }
 }
